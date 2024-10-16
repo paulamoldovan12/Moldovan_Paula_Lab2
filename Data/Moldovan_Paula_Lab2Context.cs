9958f864b1e5ce2caa5a376588427c17e73103a0
ZENBOOK\Paula <paula.a.moldovan12@gmail.com>
2024-10-16 20:34:50 +0300

    Add project files.

---
+++ Data/Moldovan_Paula_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moldovan_Paula_Lab2.Models;
+
+namespace Moldovan_Paula_Lab2.Data
+{
+    public class Moldovan_Paula_Lab2Context : DbContext
+    {
+        public Moldovan_Paula_Lab2Context (DbContextOptions<Moldovan_Paula_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Moldovan_Paula_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Moldovan_Paula_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Moldovan_Paula_Lab2.Data;
+using Moldovan_Paula_Lab2.Models;
+
+namespace Moldovan_Paula_Lab2.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Moldovan_Paula_Lab2.Data.Moldovan_Paula_Lab2Context _context;
+
+        public IndexModel(Moldovan_Paula_Lab2.Data.Moldovan_Paula_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
