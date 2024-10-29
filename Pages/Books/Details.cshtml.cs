bcfcc12c43e0fbe1120357b8fd7111879fc8ea4a
ZENBOOK\Paula <paula.a.moldovan12@gmail.com>
2024-10-29 13:53:27 +0200

    tema final

--- Pages/Books/Details.cshtml.cs
+++ Pages/Books/Details.cshtml.cs
@@ -28,17 +28,26 @@ namespace Moldovan_Paula_Lab2.Pages.Books
                 return NotFound();
             }
 
-            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
-            
-            if (book == null)
+            Book = await _context.Book
+                .Include(b => b.Author)
+                .Include(b => b.BookCategories)
+                    .ThenInclude(bc => bc.Category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+
+            // var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Book == null)
             {
                 return NotFound();
             }
+            /*
             else
             {
                 Book = book;
             }
-            
+            */
+
             return Page();
         }
     }
