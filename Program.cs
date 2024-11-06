2d0fb12bd6c1963970c53e685c2ee62dc3fd4343
ZENBOOK\Paula <paula.a.moldovan12@gmail.com>
2024-11-06 20:34:04 +0200

    lab final

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Moldovan_Paula_Lab2.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Moldovan_Paula_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Moldovan_Paula_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Moldovan_Paula_Lab2Context' not found.")));
+
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Moldovan_Paula_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Moldovan_Paula_Lab2Context' not found."))); 
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<LibraryIdentityContext>();
+
+// builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
 
